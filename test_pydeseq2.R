409016ca0e153268a929b26e55f737cd37dc13ca
mikelove <michaelisaiahlove@gmail.com>
2023-03-27 22:00:15 -0400

    working on comparions

--- test_pydeseq2.R
+++ test_pydeseq2.R
@@ -1,10 +1,44 @@
 library(DESeq2)
 set.seed(1)
 dds <- makeExampleDESeqDataSet(n=1000, m=8, betaSD=rep(0:1,c(900,100)), interceptMean=8,
-                               dispMeanRel=function(x) .1/x + 10^(rnorm(1000,-2,1)))
+                               dispMeanRel=function(x) .1/x + 10^(rnorm(1000,-4,2)))
+
+keep <- rowSums(counts(dds) >= 10) >= 4
+table(keep)
+dds <- dds[keep,]
+
+dds <- estimateSizeFactors(dds)
+dds <- estimateDispersions(dds)
+plotDispEsts(dds)
+
 dds <- DESeq(dds)
 res <- results(dds)
+lfc <- lfcShrink(dds, coef=2, quiet=TRUE)
+
 counts <- t(counts(dds))
 coldata <- as.data.frame(colData(dds)[,1,drop=FALSE])
+
 write.csv(counts, file="counts_df.csv", quote=FALSE)
 write.csv(coldata, file="clinical_df.csv", quote=FALSE)
+
+system("python3.9 test_pydeseq2.py")
+
+scan("fitted_disp_prior_var.txt")
+attr(dispersionFunction(dds), "dispPriorVar")
+
+read.csv("fitted_trend_coefs.csv", header=TRUE, row.names=1)
+attr(dispersionFunction(dds), "coefficients")
+
+disps <- read.csv("fitted_disp_table.csv", row.names=1)
+mcols(dds)[,c("dispGeneEst","dispFit","dispMAP")]
+
+plotDispEsts(dds)
+plot(res$baseMean, disps$gene, log="xy", cex=.2)
+points(res$baseMean, disps$fitted, log="xy", col="red")
+points(res$baseMean, disps$map, log="xy", col="dodgerblue")
+
+pyres <- read.csv("fitted_results_table.csv", row.names=1)
+res
+
+pylfc <- read.csv("fitted_shrunken_lfc.csv", row.names=1)
+lfc
--- test_pydeseq2.py
+++ test_pydeseq2.py
@@ -1,6 +1,8 @@
 from pydeseq2.dds import DeseqDataSet
 from pydeseq2.ds import DeseqStats
 
+import sys
+
 import pandas as pd
 counts = pd.read_csv("counts_df.csv", index_col=0)
 clinical = pd.read_csv("clinical_df.csv", index_col=0)
@@ -18,6 +20,7 @@ pd.DataFrame({"gene": dds.varm["genewise_dispersions"], "fitted": dds.varm["fitt
 
 dds.uns["trend_coeffs"].to_csv("fitted_trend_coefs.csv")
 
+# printing an ArrayView to a file
 original_stdout = sys.stdout
 f = open("fitted_disp_prior_var.txt", "w")
 sys.stdout = f
@@ -26,6 +29,9 @@ sys.stdout = original_stdout
 f.close()
 
 stat_res = DeseqStats(dds, n_cpus=1)
-stat_res.summary().to_csv("fitted_results_table.csv")
+stat_res.summary()
 
-stat_res.lfc_shrink(coeff="condition_B_vs_A").to_csv("fitted_shrunken_lfc.csv")
+stat_res.results_df.to_csv("fitted_results_table.csv")
+
+stat_res.lfc_shrink(coeff="condition_B_vs_A")
+stat_res.results_df.to_csv("fitted_shrunken_lfc.csv")
